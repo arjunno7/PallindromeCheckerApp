c711b12c815468f1ace0ad2003e757b6d3118c36
arjunno7 <ap0744@srmist.edu.in>
2026-02-26 09:54:18 +0530

    uc1 - welcome message

---
+++ app/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static  void main(String[] args){
+        System.out.println("hello");
+        System.out.println("Welcome To The PallindromeCheckerApp");
+        System.out.println("Version 1.1");
+    }
+}
